d6e0dbb4dbcc343382564657f723bee97f40e2ad
Gabe Harrington <gharrinton@lcs.on.ca>
2022-01-31 14:40:55 -0500

    Got the lines connecting the dots done next Im going to try and thicken the lines

--- Playground.playground/Pages/Empty Canvas 3.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 3.xcplaygroundpage/Contents.swift
@@ -17,7 +17,7 @@ PlaygroundPage.current.liveView = canvas
 
 
 // Show a grid
-canvas.drawAxes(withScale: true, by: 20, color: .black)
+canvas.drawAxes(withScale: true, by: 25, color: .black)
 
 // Loop from 1 to 8
 let m = -25.0
@@ -45,4 +45,10 @@ for x in stride(from: 0.0, through: 8.0, by: 1.0) {
     canvas.drawEllipse(at: Point(x: 400 - endX, y: endY), width: 5, height: 5)
     
     canvas.drawEllipse(at: Point(x: 400 - endX, y: 450 - endY), width: 5, height: 5)
+    
+    // Draw the lines conneting the dots on the rigt side
+    canvas.drawLine(from: Point(x: endX, y: endY), to: Point(x: endX, y: 450 - endY))
+    //draw the lines connecting the dots on the left side
+    canvas.drawLine(from: Point(x: 400 - endX, y: endY), to: Point(x: 400 - endX, y: 450 - endY))
 }
+
