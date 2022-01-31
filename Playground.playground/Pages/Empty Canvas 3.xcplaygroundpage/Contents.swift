faad02357c51b9b0c248c52c091c668682074b05
Gabe Harrington <gharrinton@lcs.on.ca>
2022-01-31 15:08:45 -0500

    Got the alternating thickness of the lines done

--- Playground.playground/Pages/Empty Canvas 3.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 3.xcplaygroundpage/Contents.swift
@@ -15,6 +15,17 @@ let canvas = Canvas(width: preferredWidth, height: preferredHeight)
 // Show the canvas in the playground's live view
 PlaygroundPage.current.liveView = canvas
 
+// draw box at the top
+canvas.fillColor = .init(hue: 34, saturation: 39, brightness: 87, alpha: 100)
+canvas.drawRectangle(at: Point(x: 0, y: 455), width: 400, height: 150)
+
+// Write Text in box
+//  Write Drake/Kanye
+canvas.drawText(message: "Drake/Kanye West", at: Point(x: 25, y: 550), size: 30, kerning: 0)
+// write the date
+canvas.drawText(message: "Dec/09/2021", at: Point(x: 25, y: 510), size: 30, kerning: 0)
+// write the location
+
 
 // Show a grid
 canvas.drawAxes(withScale: true, by: 25, color: .black)
@@ -46,6 +57,8 @@ for x in stride(from: 0.0, through: 8.0, by: 1.0) {
     
     canvas.drawEllipse(at: Point(x: 400 - endX, y: 450 - endY), width: 5, height: 5)
     
+    //set thickness of lines
+    canvas.defaultLineWidth = Int(9 - x)
     // Draw the lines conneting the dots on the rigt side
     canvas.drawLine(from: Point(x: endX, y: endY), to: Point(x: endX, y: 450 - endY))
     //draw the lines connecting the dots on the left side
